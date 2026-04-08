1ac3a571d643edc03db997474867bd086366446d
Roger Bertan <roger.bertan@gmail.com>
2026-04-08 20:04:24 -0300

    chore: adicionar os imports para ParameterizedTest e ValueSource

--- src/test/java/br/com/ofisy/domain/customer/CpfCnpjTest.java
+++ src/test/java/br/com/ofisy/domain/customer/CpfCnpjTest.java
@@ -3,6 +3,8 @@ package br.com.ofisy.domain.customer;
 import br.com.ofisy.domain.customer.exceptions.InvalidCpfCnpjException;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
