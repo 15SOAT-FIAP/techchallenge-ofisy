3c21e0b9cd87f748b927497ff65fe3273cff922f
Juliana <juliana.costa@discover.com.br>
2026-04-10 00:00:00 -0300

    feat: Classes de aplicação e infraestrutura do usuário

--- src/main/java/br/com/ofisy/infrastructure/config/swagger/SwaggerConfig.java
+++ src/main/java/br/com/ofisy/infrastructure/config/swagger/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package br.com.ofisy.infrastructure.config;
+package br.com.ofisy.infrastructure.config.swagger;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
