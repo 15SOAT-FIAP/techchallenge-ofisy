40944611fff6e2d6324fd0be5f3a1b9771761a8d
Juliana <juliana.costa@discover.com.br>
2026-04-10 00:00:00 -0300

    feat: Classes de aplicação e infraestrutura do usuário

--- src/main/java/br/com/ofisy/infrastructure/config/security/SecurityConfig.java
+++ src/main/java/br/com/ofisy/infrastructure/config/security/SecurityConfig.java
@@ -35,6 +35,11 @@ public class SecurityConfig {
     }
 
     @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
