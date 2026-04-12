605d8c0401e9c43be1a52576b6d7b70856d6e30b
Kalel <kalelfleith03@gmail.com>
2026-04-11 21:18:51 -0300

    feat: Exceptions StockNotFoundException e InsufficientStockException

---
+++ src/main/java/br/com/ofisy/application/stock/exceptions/InsufficientStockException.java
@@ -0,0 +1,9 @@
+package br.com.ofisy.application.stock.exceptions;
+
+import java.util.UUID;
+
+public class InsufficientStockException extends RuntimeException {
+    public InsufficientStockException(UUID stockId) {
+        super("Estoque insuficiente: ID" + stockId);
+    }
+}
