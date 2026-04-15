66611f25df236216a11da5ffac8b6ba0e2b93a63
Roger Bertan <roger.bertan@gmail.com>
2026-04-14 21:23:51 -0300

    refactor: alterar endpoint de busca por cpfCnpj para aceitar passando por parametro

--- src/main/java/br/com/ofisy/interfaces/api/customer/CustomerController.java
+++ src/main/java/br/com/ofisy/interfaces/api/customer/CustomerController.java
@@ -11,12 +11,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -40,8 +35,9 @@ public class CustomerController {
         return ResponseEntity.ok(customerService.identifyCustomerById(id));
     }
 
-    @GetMapping("/cpfcnpj/{cpfCnpj}")
-    public ResponseEntity<CustomerResponseDTO> getCustomerByCpfCnpj(@PathVariable String cpfCnpj) {
+    @GetMapping(params = "cpfCnpj")
+    public ResponseEntity<CustomerResponseDTO> getCustomerByCpfCnpj(
+            @RequestParam(required = false) String cpfCnpj) {
 
         return ResponseEntity.ok(customerService.identifyCustomerByCpfCnpj(cpfCnpj));
     }
