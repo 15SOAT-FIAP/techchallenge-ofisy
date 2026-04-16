e502d7553f7483d7cbd9efad39c6493b83e7ef8b
Juliana <juliana.costa@discover.com.br>
2026-04-16 10:22:50 -0300

    feat: Melhoria em implementação de busca por lista no UserController

--- src/main/java/br/com/ofisy/interfaces/api/user/UserController.java
+++ src/main/java/br/com/ofisy/interfaces/api/user/UserController.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -41,7 +42,7 @@ public class UserController {
     @Operation(summary = "Listar todos os usuários")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Page<UserResponseDTO>> listAllUsers(
-            @PageableDefault(size = 10, sort = "name", direction = Sort.Direction.ASC) Pageable pageable) {
+            @ParameterObject @PageableDefault(size = 10) Pageable pageable) {
         return ResponseEntity.ok(userService.listAllUsers(pageable));
     }
 
