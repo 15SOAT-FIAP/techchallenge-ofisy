e89b0055aa468ea41bba816e74b3c5bd1b56ad16
Matheus SIlva <mc75071@gmail.com>
2026-04-17 19:53:16 -0300

    feat: enhance NotificationRepository with additional query methods
    (cherry picked from commit 3ea96deccb9dde372da131f22c37c42638fba991)

--- src/main/java/br/com/ofisy/domain/notification/NotificationRepository.java
+++ src/main/java/br/com/ofisy/domain/notification/NotificationRepository.java
@@ -1,15 +1,19 @@
 package br.com.ofisy.domain.notification;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface NotificationRepository {
 
     Notification save(Notification notification);
 
+    List<Notification> findAll();
+
     List<Notification> findAllUnread();
+
+    Optional<Notification> findById(UUID id);
 
     List<Notification> findByStockId(UUID stockId);
 
-//    List<Notification> findByServiceOrderId(UUID serviceOrderId);
 }
