eb0101d47d3e7fcfc2b65f229ff4480ec5633bf8
Juliana <juliana.costa@discover.com.br>
2026-04-16 21:16:24 -0300

    feat: Adição dos demais códigos de retorno para APIs para documentação no Swagger

--- src/main/java/br/com/ofisy/interfaces/api/user/UserController.java
+++ src/main/java/br/com/ofisy/interfaces/api/user/UserController.java
@@ -1,24 +1,33 @@
 package br.com.ofisy.interfaces.api.user;
 
-import br.com.ofisy.application.user.dto.UserDTO.*;
-import br.com.ofisy.application.user.service.UserService;
+import br.com.ofisy.application.user.UserService;
+import br.com.ofisy.application.user.dto.CreateUserRequestDTO;
+import br.com.ofisy.application.user.dto.ModifyUserRoleRequestDTO;
+import br.com.ofisy.application.user.dto.UpdatePasswordRequestDTO;
+import br.com.ofisy.application.user.dto.UserResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.UUID;
 
 @RestController()
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
-@Tag(name = "Usuários")
+@Tag(name = "CRUD Usuários")
 @SecurityRequirement(name = "Bearer Token")
 public class UserController {
 
@@ -27,61 +36,61 @@ public class UserController {
     @PostMapping
     @Operation(summary = "Criar novo usuário")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<UserResponse> createUser(@RequestBody @Valid CreateUserRequest request) {
+    public ResponseEntity<UserResponseDTO> createUser(@RequestBody @Valid CreateUserRequestDTO request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.create(request));
     }
 
     @GetMapping
     @Operation(summary = "Listar todos os usuários")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<List<UserResponse>> listAllUsers() {
-        return ResponseEntity.ok(userService.listAllUsers());
+    public ResponseEntity<Page<UserResponseDTO>> listAllUsers(
+            @ParameterObject @PageableDefault(size = 10) Pageable pageable) {
+        return ResponseEntity.ok(userService.listAllUsers(pageable));
     }
 
     @GetMapping("/{id}")
     @Operation(summary = "Buscar usuário por ID")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<UserResponse> findById(@PathVariable UUID id) {
-        UserResponse userResponse = userService.findById(id);
-        return ResponseEntity.ok(userResponse);
+    public ResponseEntity<UserResponseDTO> findById(@PathVariable UUID id) {
+        return ResponseEntity.ok(userService.findById(id));
     }
 
     @PatchMapping("/{id}/modify-role")
     @Operation(summary = "Alterar role do usuário")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<UserResponse> modifyRole(
+    public ResponseEntity<UserResponseDTO> modifyRole(
             @PathVariable UUID id,
-            @RequestBody @Valid ModifyUserRoleRequest request) {
+            @RequestBody @Valid ModifyUserRoleRequestDTO request) {
         return ResponseEntity.ok(userService.modifyUserRole(id, request));
     }
 
     @PatchMapping("/{id}/update-password")
     @Operation(summary = "Alterar senha do usuário")
-    @PreAuthorize("hasRole('ADMIN') or #id == authentication.principal.id")
-    public ResponseEntity<UserResponse> modifyPassword(
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<UserResponseDTO> modifyPassword(
             @PathVariable UUID id,
-            @RequestBody @Valid UpdatePasswordRequest request) {
+            @RequestBody @Valid UpdatePasswordRequestDTO request) {
         return ResponseEntity.ok(userService.updatePassword(id, request));
     }
 
     @PatchMapping("/{id}/deactivate")
     @Operation(summary = "Desativar usuário")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<UserResponse> deactivateUser(@PathVariable UUID id) {
+    public ResponseEntity<UserResponseDTO> deactivateUser(@PathVariable UUID id) {
         return ResponseEntity.ok(userService.deactivateUser(id));
     }
 
     @PatchMapping("/{id}/activate")
     @Operation(summary = "Ativar usuário")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<UserResponse> activateUser(@PathVariable UUID id) {
+    public ResponseEntity<UserResponseDTO> activateUser(@PathVariable UUID id) {
         return ResponseEntity.ok(userService.activateUser(id));
     }
 
     @DeleteMapping("/{id}/remove")
     @Operation(summary = "Remover usuário")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<UserResponse> removeUser(@PathVariable UUID id) {
+    public ResponseEntity<Void> removeUser(@PathVariable UUID id) {
         userService.removeUser(id);
         return ResponseEntity.noContent().build();
     }
