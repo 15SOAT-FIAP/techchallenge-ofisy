b3c7f530531d498eeb2a9dc675914d9cc0b70cfa
Juliana <juliana.costa@discover.com.br>
2026-04-18 21:06:30 -0300

    feat: Adicionando padrão de interface Api para Login

--- src/main/java/br/com/ofisy/interfaces/api/user/LoginController.java
+++ src/main/java/br/com/ofisy/interfaces/api/user/LoginController.java
@@ -3,11 +3,6 @@ package br.com.ofisy.interfaces.api.user;
 import br.com.ofisy.application.user.dto.LoginRequestDTO;
 import br.com.ofisy.application.user.dto.LoginResponseDTO;
 import br.com.ofisy.infrastructure.config.security.JwtService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,17 +14,12 @@ import org.springframework.web.bind.annotation.*;
 @RestController()
 @RequestMapping("/api/v1/login")
 @RequiredArgsConstructor
-@Tag(name = "Autenticação via JWT Token para APIs administrativas")
-public class LoginController {
+public class LoginController implements LoginApi {
 
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
 
     @PostMapping
-    @Operation(summary = "Realizar login e obter o token JWT")
-    @ApiResponse(responseCode = "200", description = "Login realizado com sucesso, token JWT retornado")
-    @ApiResponse(responseCode = "400", description = "Dados inválidos", content = @Content(schema = @Schema(implementation = Void.class)))
-    @ApiResponse(responseCode = "401", description = "Email ou senha inválidos", content = @Content(schema = @Schema(implementation = Void.class)))
     public ResponseEntity<LoginResponseDTO> login(@Valid @RequestBody LoginRequestDTO request) {
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.email(), request.password()));
         String token = jwtService.generateToken(authentication.getName());
