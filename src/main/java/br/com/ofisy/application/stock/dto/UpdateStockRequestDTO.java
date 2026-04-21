eeae3b4c89251e03006e45179bc159d3f06d54dd
Kalel <kalelfleith03@gmail.com>
2026-04-21 17:35:53 -0300

    feat: Adiciona UpdateStockRequestDTO.java

---
+++ src/main/java/br/com/ofisy/application/stock/dto/UpdateStockRequestDTO.java
@@ -0,0 +1,22 @@
+package br.com.ofisy.application.stock.dto;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+
+import java.math.BigDecimal;
+
+public record UpdateStockRequestDTO(
+
+        String productName,
+
+        String description,
+
+        @DecimalMin(value = "0.0", inclusive = false, message = "Preço deve ser maior que zero")
+        BigDecimal unitPrice,
+
+        String category,
+
+        @Min(value = 0, message = "Limite mínimo não pode ser negativo")
+        Integer minThreshold
+) {
+}
