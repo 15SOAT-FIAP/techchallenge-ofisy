6d11c1bb5035045d630d82ed39606bac57fd8659
Kalel <kalelfleith03@gmail.com>
2026-04-21 09:49:26 -0300

    revert: Não atualiza migration

--- src/main/resources/db/migration/V1__create_stocks_table.sql
+++ src/main/resources/db/migration/V1__create_stocks_table.sql
@@ -2,9 +2,9 @@ CREATE TABLE stocks
 (
     id            UUID PRIMARY KEY        DEFAULT gen_random_uuid(),
     product_name  VARCHAR(255)   NOT NULL,
-    description   VARCHAR(255)   NOT NULL,
+    description   VARCHAR(255),
     quantity      INTEGER        NOT NULL DEFAULT 0,
-    category      VARCHAR(255)   NOT NULL,
+    category      VARCHAR(255),
     unit_price    DECIMAL(10, 2) NOT NULL,
     min_threshold INTEGER        NOT NULL DEFAULT 0,
     created_at    TIMESTAMP      NOT NULL DEFAULT now(),
