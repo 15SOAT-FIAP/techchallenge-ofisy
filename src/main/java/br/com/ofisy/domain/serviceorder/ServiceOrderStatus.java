0df0dba05e18a19aaaf205a21fb2b4b86f5f1e2a
Roger Bertan <roger.bertan@gmail.com>
2026-04-23 22:18:10 -0300

    feat: implementar estrutura inicial de dominio para service order

---
+++ src/main/java/br/com/ofisy/domain/serviceorder/ServiceOrderStatus.java
@@ -0,0 +1,12 @@
+package br.com.ofisy.domain.serviceorder;
+
+public enum ServiceOrderStatus {
+    RECEIVED,
+    IN_DIAGNOSTIC,
+    AWAITING_APPROVAL,
+    AWAITING_EXECUTION,
+    IN_PROGRESS,
+    FINISHED,
+    SHIPPED,
+    CANCELLED,
+}
