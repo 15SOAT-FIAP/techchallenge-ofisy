34770f359392bd89d8e14331f0d48699319bc9bf
rogerbertan <roger.bertan@gmail.com>
2026-04-26 02:13:14 -0300

    feat: adicionar findById ao repositorio de ordem de servico

--- src/main/java/br/com/ofisy/domain/serviceorder/ServiceOrderRepository.java
+++ src/main/java/br/com/ofisy/domain/serviceorder/ServiceOrderRepository.java
@@ -1,5 +1,9 @@
 package br.com.ofisy.domain.serviceorder;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface ServiceOrderRepository {
     ServiceOrder save(ServiceOrder serviceOrder);
+    Optional<ServiceOrder> findById(UUID id);
 }
