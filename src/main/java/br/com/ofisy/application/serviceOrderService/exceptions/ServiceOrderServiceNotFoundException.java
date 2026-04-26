a64e782486372ff3ea9806c5912b59a3214a7939
kalelfleith <60950449+Kalelfleith@users.noreply.github.com>
2026-04-26 12:42:54 -0300

    Revert "Feature/9 crud de serviços"

--- src/main/java/br/com/ofisy/application/serviceOrderService/exceptions/ServiceOrderServiceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package br.com.ofisy.application.serviceOrderService.exceptions;
-
-public class ServiceOrderServiceNotFoundException extends RuntimeException {
-    public ServiceOrderServiceNotFoundException(String id) {
-        super("Serviço não encontrado com ID: " + id);
-    }
-}
-
