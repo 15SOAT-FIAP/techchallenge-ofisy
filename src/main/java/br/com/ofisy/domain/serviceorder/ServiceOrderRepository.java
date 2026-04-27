97be5efe4229f4ea8197a14e1f51bb42b4a5f4e3
Roger Bertan <roger.bertan@gmail.com>
2026-04-27 00:52:25 -0300

    feat: adicionar consulta de ordens de servico por status no repositorio

--- src/main/java/br/com/ofisy/domain/serviceorder/ServiceOrderRepository.java
+++ src/main/java/br/com/ofisy/domain/serviceorder/ServiceOrderRepository.java
@@ -1,9 +1,13 @@
 package br.com.ofisy.domain.serviceorder;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import java.util.Optional;
 import java.util.UUID;
 
 public interface ServiceOrderRepository {
     ServiceOrder save(ServiceOrder serviceOrder);
     Optional<ServiceOrder> findById(UUID id);
+    Page<ServiceOrder> findByStatus(ServiceOrderStatus serviceOrderStatus, Pageable pageable);
 }
