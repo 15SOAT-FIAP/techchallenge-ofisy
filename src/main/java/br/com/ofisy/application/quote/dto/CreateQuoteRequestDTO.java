ef290159b8b0d5279174ca636565bd7f780e2994
Juliana <juliana.costa@discover.com.br>
2026-04-27 20:19:36 -0300

    feat: Ajustes em validações dos dtos não obrigatórios

--- src/main/java/br/com/ofisy/application/quote/dto/CreateQuoteRequestDTO.java
+++ src/main/java/br/com/ofisy/application/quote/dto/CreateQuoteRequestDTO.java
@@ -11,8 +11,6 @@ public record CreateQuoteRequestDTO(
         @NotNull(message = "ID da ordem de serviço é obrigatório")
         UUID serviceOrderId,
 
-        @NotNull(message = "Itens de estoque são obrigatórios")
-        @NotEmpty(message = "O orçamento deve ter pelo menos um item de estoque")
         @Valid
         List<StockItemRequestDTO> stockItems,
 
