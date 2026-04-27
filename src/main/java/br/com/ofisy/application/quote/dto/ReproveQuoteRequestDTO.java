dbec9566df36883595add7e805f0c80d4aad37db
Juliana <juliana.costa@discover.com.br>
2026-04-27 20:19:36 -0300

    feat: Ajustes em validações dos dtos não obrigatórios

--- src/main/java/br/com/ofisy/application/quote/dto/ReproveQuoteRequestDTO.java
+++ src/main/java/br/com/ofisy/application/quote/dto/ReproveQuoteRequestDTO.java
@@ -1,8 +1,5 @@
 package br.com.ofisy.application.quote.dto;
 
-import jakarta.validation.constraints.NotNull;
-
 public record ReproveQuoteRequestDTO(
-        @NotNull(message = "Motivo da reprovação é obrigatório")
         String reason
 ) {}
