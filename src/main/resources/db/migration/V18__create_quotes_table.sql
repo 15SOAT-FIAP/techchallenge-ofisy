c1bffceaf0d112df21634088e0d74ca582145348
Juliana <juliana.costa@discover.com.br>
2026-04-28 22:10:57 -0300

    feat: Migrations de criação de tabelas para orçamento

---
+++ src/main/resources/db/migration/V18__create_quotes_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE quotes
+(
+    id                   UUID        PRIMARY KEY DEFAULT gen_random_uuid(),
+    service_order_id     UUID        NOT NULL,
+    status               VARCHAR(20) NOT NULL DEFAULT 'PENDING',
+    total_price          DECIMAL(10, 2) NOT NULL,
+    quote_refusal_reason TEXT,
+    created_at           TIMESTAMP   NOT NULL DEFAULT now(),
+    updated_at           TIMESTAMP,
+
+    CONSTRAINT fk_quotes_service_order
+        FOREIGN KEY (service_order_id)
+            REFERENCES service_orders (id)
+);
