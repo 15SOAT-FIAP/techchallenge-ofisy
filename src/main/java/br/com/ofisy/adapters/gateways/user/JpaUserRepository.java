76fdc39fcbdd32947940dc9aa1a51de487432485
Juliana <juliana.costa@discover.com.br>
2026-06-10 07:47:22 -0300

    feat: Refatorando classes de domínio e repositórios

---
+++ src/main/java/br/com/ofisy/adapters/gateways/user/JpaUserRepository.java
@@ -0,0 +1,13 @@
+package br.com.ofisy.adapters.gateways.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface JpaUserRepository extends JpaRepository<UserEntity, UUID> {
+
+    Optional<UserEntity> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
