fb4faff54908af96a76cd28ca3713238e5b74912
Victoria <victoria.marques8424@gmail.com>
2026-07-10 14:39:40 -0300

    fix: remoção de outputs desnecessários

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -2,64 +2,10 @@
 # OUTPUTS
 ########################################
 
-# Exibe o ID da VPC criada.
-output "vpc_id" {
-  description = "ID da VPC criada"
-  value       = aws_vpc.main.id
-}
-
-# Exibe o bloco CIDR configurado para a VPC.
-output "vpc_cidr" {
-  description = "CIDR block da VPC"
-  value       = aws_vpc.main.cidr_block
-}
-
-# Exibe o ID da subnet pública A.
-output "public_subnet_a_id" {
-  description = "ID da subnet pública A"
-  value       = aws_subnet.public_a.id
-}
-
-# Exibe o ID da subnet pública B.
-output "public_subnet_b_id" {
-  description = "ID da subnet pública B"
-  value       = aws_subnet.public_b.id
-}
-
-# Exibe o ID da subnet privada A.
-output "private_subnet_a_id" {
-  description = "ID da subnet privada A"
-  value       = aws_subnet.private_a.id
-}
-
-# Exibe o ID da subnet privada B.
-output "private_subnet_b_id" {
-  description = "ID da subnet privada B"
-  value       = aws_subnet.private_b.id
-}
-
-# Exibe o ID do Security Group utilizado pelo cluster EKS.
-output "eks_security_group_id" {
-  description = "ID do Security Group do EKS"
-  value       = aws_security_group.eks.id
-}
-
-# Exibe o ID do Security Group utilizado pelo banco RDS.
-output "rds_security_group_id" {
-  description = "ID do Security Group do RDS"
-  value       = aws_security_group.rds.id
-}
-
 # Exibe a URL do repositório Amazon ECR.
 output "ecr_repository_url" {
   description = "URL do repositório ECR"
   value       = aws_ecr_repository.main.repository_url
-}
-
-# Exibe o nome do repositório Amazon ECR.
-output "ecr_repository_name" {
-  description = "Nome do repositório ECR"
-  value       = aws_ecr_repository.main.name
 }
 
 # Exibe o endpoint de conexão da instância RDS.
@@ -99,22 +45,10 @@ output "eks_cluster_name" {
   value       = aws_eks_cluster.main.name
 }
 
-# Exibe o ARN do cluster Amazon EKS.
-output "eks_cluster_arn" {
-  description = "ARN do cluster EKS"
-  value       = aws_eks_cluster.main.arn
-}
-
 # Exibe o endpoint da API do cluster Amazon EKS.
 output "eks_cluster_endpoint" {
   description = "Endpoint do API Server do cluster EKS"
   value       = aws_eks_cluster.main.endpoint
-}
-
-# Exibe a versão do Kubernetes utilizada pelo cluster.
-output "eks_cluster_version" {
-  description = "Versão do Kubernetes no cluster EKS"
-  value       = aws_eks_cluster.main.version
 }
 
 # Exibe o certificado da Autoridade Certificadora (CA) do cluster EKS.
@@ -128,30 +62,6 @@ output "eks_cluster_certificate_authority" {
 output "eks_cluster_security_group_id" {
   description = "ID do Security Group do cluster EKS"
   value       = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
-}
-
-# Exibe o nome do Node Group do EKS.
-output "eks_node_group_name" {
-  description = "Nome do Node Group"
-  value       = aws_eks_node_group.main.node_group_name
-}
-
-# Exibe o ARN do Node Group do EKS.
-output "eks_node_group_arn" {
-  description = "ARN do Node Group"
-  value       = aws_eks_node_group.main.arn
-}
-
-# Exibe o status do Node Group do EKS.
-output "eks_node_group_status" {
-  description = "Status do Node Group"
-  value       = aws_eks_node_group.main.status
-}
-
-# Exibe o nome configurado para o projeto.
-output "project_name" {
-  description = "Nome do projeto"
-  value       = local.project_name
 }
 
 # Exibe a região da AWS utilizada na infraestrutura.
