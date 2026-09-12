cd10435e26ce2a0916f6aba8d7b08a7256e9f036
Kalel <kalelfleith03@gmail.com>
2026-09-12 11:09:47 -0300

    test: fornecer MeterRegistry real nos testes de controller

--- src/test/java/br/com/ofisy/interfaces/api/ControllerTestBase.java
+++ src/test/java/br/com/ofisy/interfaces/api/ControllerTestBase.java
@@ -2,10 +2,16 @@ package br.com.ofisy.interfaces.api;
 
 import br.com.ofisy.shared.jwt.JwtService;
 import br.com.ofisy.shared.securityfilter.OfisyUserDetailsService;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @AutoConfigureMockMvc(addFilters = false)
+@Import(ControllerTestBase.MetricsTestConfig.class)
 public abstract class ControllerTestBase {
 
     @MockitoBean
@@ -13,4 +19,13 @@ public abstract class ControllerTestBase {
 
     @MockitoBean
     protected OfisyUserDetailsService userDetailsService;
+
+    @TestConfiguration
+    static class MetricsTestConfig {
+
+        @Bean
+        MeterRegistry meterRegistry() {
+            return new SimpleMeterRegistry();
+        }
+    }
 }
